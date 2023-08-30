b1e8c749c36d4a2c49c12e2b6baf58d8d983e4eb
Xiaohuba <175194786@qq.com>
2023-08-30 09:28:34 +0800

    support spj

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import utils
 
 parser = argparse.ArgumentParser(description="Parse Lemonlime conf files(.cdf)")
 parser.add_argument("filename", type=str, help="Lemon work path")
+parser.add_argument("--silent", type=bool, help="Silent mode")
 args = parser.parse_args()
 
 lemonDir = os.path.abspath(args.filename)
+silent = args.silent
 cdfPath = utils.getCDFPath(lemonDir)
 
-print(f"Load .cdf file: {cdfPath}")
+if cdfPath == None:
+    print("ERROR: No .cdf file found!")
+    print("Exiting...")
+    exit(1)
+
+print(f"INFO: Load .cdf file: {cdfPath}\n")
 
 with open(cdfPath, "r") as cdf:
     json_obj = json.load(cdf)
@@ -18,67 +25,88 @@ with open(cdfPath, "r") as cdf:
         os.mkdir("to_uoj")
     except Exception as exp:
         print(
-            f"Failed to create folder `to_uoj`.\nReceived Exception {exp}.\nRetriing..."
+            f"WARNING: Failed to create folder `to_uoj`.\nReceived Exception {exp}.\nRetriing..."
         )
         os.rename("to_uoj", f"to_uoj.{random.randint(1,9999)}.old")
         os.mkdir("to_uoj")
     for task in tasks:
-        taskname = task["problemTitle"]
-        print(f"Parsing task {taskname}...")
-        os.mkdir(os.path.join("to_uoj", taskname))
-        testcases = task["testCases"]
-        conf = ""
-        cnt = 0
-        id = 0
-        score = 0
-        tl = 0
-        ml = 0
-        conf += f"input_pre {taskname}\n"
-        conf += f"input_suf in\n"
-        conf += f"output_pre {taskname}\n"
-        conf += f"output_suf out\n"
-        conf += f"n_subtasks {len(testcases)}\n"
-        for case in testcases:
-            id += 1
-            score += case["fullScore"]
-            conf += f"subtask_end_{id} {cnt + len(case['inputFiles'])}\n"
-            conf += f"subtask_score_{id} {case['fullScore']}\n"
-            tl = max(tl, case["timeLimit"])
-            ml = max(ml, case["memoryLimit"])
-            caseid = 0
-            for inf in case["inputFiles"]:
-                fr = "data" + utils.parsePath(inf)
-                caseid += 1
-                to = os.path.join(
-                    "to_uoj",
-                    taskname,
-                    f"{task['problemTitle']}{caseid + cnt}.in",
-                )
-                print(fr, "->", to)
-                shutil.copy2(fr, to)
-            caseid = 0
-            for ouf in case["outputFiles"]:
-                fr = "data" + utils.parsePath(ouf)
-                caseid += 1
-                to = os.path.join(
-                    "to_uoj",
-                    taskname,
-                    f"{task['problemTitle']}{caseid + cnt}.out",
-                )
-                print(fr, "->", to)
-                shutil.copy2(fr, to)
-            cnt += len(case["inputFiles"])
-        conf += f"n_tests {cnt}\n"
-        conf += f"n_ex_tests 0\n"
-        conf += f"n_sample_tests 0\n"
-        conf += f"time_limit_ms {tl}\n"
-        conf += f"memory_limit {ml}\n"
-        if len(task["specialJudge"]) == 0:
-            conf += f"use_builtin_checker wcmp\n"
-        else:
-            print("This task has spj.\nYou may need to edit it manually.")
-        conf += f"use_builtin_judger on\n"
-        confFile = open(os.path.join("to_uoj", taskname, "problem.conf"), "w")
-        confFile.write(conf)
-        confFile.close()
-        print("\n\n")
+        try:
+            taskname = task["problemTitle"]
+            print(f"INFO: Parsing task {taskname}...")
+            os.mkdir(os.path.join("to_uoj", taskname))
+            testcases = task["testCases"]
+            conf = ""
+            cnt = 0
+            id = 0
+            score = 0
+            tl = 0
+            ml = 0
+            conf += f"input_pre {taskname}\n"
+            conf += f"input_suf in\n"
+            conf += f"output_pre {taskname}\n"
+            conf += f"output_suf out\n"
+            conf += f"n_subtasks {len(testcases)}\n"
+            for case in testcases:
+                id += 1
+                score += case["fullScore"]
+                conf += f"subtask_end_{id} {cnt + len(case['inputFiles'])}\n"
+                conf += f"subtask_score_{id} {case['fullScore']}\n"
+                tl = max(tl, case["timeLimit"])
+                ml = max(ml, case["memoryLimit"])
+                caseid = 0
+                for inf in case["inputFiles"]:
+                    fr = "data" + utils.parsePath(inf)
+                    caseid += 1
+                    to = os.path.join(
+                        "to_uoj",
+                        taskname,
+                        f"{task['problemTitle']}{caseid + cnt}.in",
+                    )
+                    if not silent:
+                        print("INFO: Copying", fr, "->", to)
+                    shutil.copy2(fr, to)
+                caseid = 0
+                for ouf in case["outputFiles"]:
+                    fr = "data" + utils.parsePath(ouf)
+                    caseid += 1
+                    to = os.path.join(
+                        "to_uoj",
+                        taskname,
+                        f"{task['problemTitle']}{caseid + cnt}.out",
+                    )
+                    if not silent:
+                        print("INFO: Copying", fr, "->", to)
+                    shutil.copy2(fr, to)
+                cnt += len(case["inputFiles"])
+            conf += f"n_tests {cnt}\n"
+            conf += f"n_ex_tests 0\n"
+            conf += f"n_sample_tests 0\n"
+            conf += f"time_limit_ms {tl}\n"
+            conf += f"memory_limit {ml}\n"
+            if len(task["specialJudge"]) == 0:
+                conf += f"use_builtin_checker wcmp\n"
+                if not silent:
+                    print(f"INFO: Using `wcmp` for task {taskname}.")
+            else:
+                (state, text) = utils.parseSPJ(os.path.join(lemonDir, "data", taskname))
+                # print(f"LOG: {os.path.join(lemonDir, 'data', taskname)}")
+                if state == 0:
+                    if not silent:
+                        print(f"INFO: Parsing spj for task {taskname}...")
+                    spjFile = open(os.path.join("to_uoj", taskname, "chk.cpp"), "w")
+                    spjFile.write(text)
+                    spjFile.close()
+                else:
+                    print("WARNING: This task has unsupported spj.")
+                    print("You may need to edit it manually.")
+            conf += f"use_builtin_judger on\n"
+            if task["taskType"] != 0:
+                print("WARNING: Unsupported task type.")
+                print("You may need to configure it manually.")
+            confFile = open(os.path.join("to_uoj", taskname, "problem.conf"), "w")
+            confFile.write(conf)
+            confFile.close()
+            print("\n\n")
+        except Exception as exp:
+            print(f"ERROR: Unhandled Exception {exp}")
+            print("Passing...")
--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ def getCDFPath(pth):
     for file in files:
         if file.endswith(".cdf"):
             return os.path.join(pth, file)
+    return None
 
 
 def parsePath(pth):
@@ -26,7 +27,19 @@ def parsePath(pth):
     return final
 
 
-if __name__ == "__main__":
-    print(parsePath("a\\b"))
-    print(parsePath("a/b\\c"))
-    print(parsePath("casio/casio1.in/"))
+def parseSPJ(pth):
+    files = os.listdir(pth)
+    for file in files:
+        # print(f"{file}")
+        if file == "chk.cpp" or file == "checker.cpp":
+            with open(os.path.join(pth, file), "r") as spj:
+                content = spj.read()
+                content = content.replace("testlib_for_lemon.h", "testlib.h")
+                content = content.replace("testlib_for_lemons.h", "testlib.h")
+                content = content.replace("registerLemonChecker", "registerTestlibCmd")
+                # print(content)
+                if content.find("testlib.h") == -1:
+                    return (-1, "")
+                else:
+                    return (0, content)
+    return (-1, "")
