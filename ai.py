8facaa7c0e9e3da9947a07c3bc6288e9d0dab0cb
Xiaohuba <175194786@qq.com>
2025-09-24 16:07:08 +0800

    feat: change model to GPT5-mini and tweak prompt

--- ai.py
+++ ai.py
@@ -2,12 +2,12 @@ import openai
 import os
 
 
-prompt = """请将图片内容转为规范的 markdown+latex，标题从一级开始。
+prompt = """请将图片内容转为规范的 markdown，题目名使用三级标题，每节（如题目描述、输入格式等）小标题使用四级标题。
 特别的：
 - 请忽略所有的页眉、页脚和代码框的行号。
 - 对于表格中跨越多行的单元格，请在 markdown 表格中每行都重复一遍。
 - 请将图片中的所有公式、数字转为 latex 格式并使用美元符号包裹。
-- 请将图片中的文件名、命令行参数、样例输入输出使用 markdown 代码块包裹。
+- 请将图片中的命令行参数、样例输入输出使用 markdown 代码块包裹。
 - 请直接输出转化完成的 markdown 文本，不要输出任何多余文字。"""
 
 
@@ -25,7 +25,7 @@ async def image2md(base64_images):
         for base64_image in base64_images
     ]
     completion = await client.chat.completions.create(
-        model="google/gemini-2.5-flash",
+        model="openai/gpt-5-mini",
         messages=[
             {
                 "role": "system",
